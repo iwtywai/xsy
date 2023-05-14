d41317dd4fab68cd55895e820edda14936cd5352
iwtywai <25090836+iwtywai@users.noreply.github.com>
2023-05-14 15:20:29 +0800

    Add files via upload

---
+++ xsy.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+str = '''Founder of a voluntary organization called \"Sharing English Sharing Love\". In particular, I hold an English corner every week--- sharing reading experiences, recommending books to read, and teaching younger children about English grammar and sentence structures. Moreover, we went to a small village in Anhui province, a province in China, and teach kids their English in the summer of 2019. Unfortunately, we did not continue the program in the following years due to the wide spread of the Covid-19 pandemic and the kid\' lack of access to the internet, making online classes an impossibility for the children. It is heart-breaking to learn the tremendous gap in education, enhancing my determination to narrowing, even eliminating, the inequalities in education. As we are entering a post-pandemic era, I will continue to travel to rural areas this summer to spread the charm of English and knowledge.'''
+
+st.header("Awesome things about myself")
+st.write(str)
